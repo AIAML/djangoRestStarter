ea737e9f4e544c59b315f71200193a8086a808a2
majid hemmati <majid.hemmati@yahoo.com>
2026-01-06 23:05:36 +0330

    Initial commit: project structure with apps users and documents

--- djangoRestStarter/settings.py
+++ djangoRestStarter/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users.apps.UsersConfig',
+    'apps.core.apps.CoreConfig',
+    'apps.documents.apps.DocumentsConfig',
 ]
 
 MIDDLEWARE = [
