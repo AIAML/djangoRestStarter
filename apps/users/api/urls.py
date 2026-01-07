63e1318938259feeb7feb394d6ae997c006d7082
majid hemmati <majid.hemmati@yahoo.com>
2026-01-07 12:36:19 +0330

    Project Has been built

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 
 from .views import RegisterView,UserProfileView
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [path("register/", RegisterView.as_view(),name='user-register'),
                path('profile/', UserProfileView.as_view(),name='user-profile'),
+               path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # login
+               path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
                ]
